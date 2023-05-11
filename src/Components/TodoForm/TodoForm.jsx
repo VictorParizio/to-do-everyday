f6fa2f51eb223908e889459f2a88230ccfb2dd90
Victor Parizio <victorparizio@hotmail.com>
2023-05-11 09:44:15 -0300

    feat (App, TodoForm) Adição de nova tarefa Foi adicionada uma função para adicionar nova tarefa, que após ser adicionada limpa os campos do formaulário e criada uma condição para não aceitar novas tarefas sem nome ou categoria.

--- src/Components/TodoForm/TodoForm.jsx
+++ src/Components/TodoForm/TodoForm.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function TodoForm(){
-    return(
+export default function TodoForm({ addTodo }) {
+    const [value, setValue] = useState("")
+    const [category, setCategory] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!value || !category) return
+        addTodo(value, category)
+        setValue("")
+        setCategory("")
+    }
+
+    return (
         <section className="todo-form">
             <h2>Nova Tarefa:</h2>
-            <form>
-                <input type="text" placeholder="Digite a Tarefa" />
-                <select>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Digite a Tarefa"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                />
+                <select
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                >
                     <option value="">Selecione uma categoria</option>
                     <option value="Trabalho">Trabalho</option>
                     <option value="Pessoal">Pessoal</option>
